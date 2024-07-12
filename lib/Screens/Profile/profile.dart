3f1e80cf02b0dc0892978993cdf83630723269fc
Your Name <you@example.com>
2024-07-12 23:10:39 +0530

    basic changes

--- lib/Screens/Profile/profile.dart
+++ lib/Screens/Profile/profile.dart
@@ -10,174 +10,150 @@ class Profile extends StatelessWidget {
 
   final User? user = Auth().currentUser;
 
-  Widget _userUID(){
-    return Text(user?.email?? 'User email',
+  Widget _userUID() {
+    return Text(
+      user?.email ?? 'User email',
       style: const TextStyle(
         color: Colors.blue,
       ),
     );
   }
 
-  Future<void> signoutttt() async{
+  Future<void> signoutttt() async {
     await Auth().signTFOut();
   }
 
-  Widget _signoutButt(){
+  Widget _signoutButt() {
     return ElevatedButton(
       onPressed: signoutttt,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white, backgroundColor: Colors.redAccent,
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
       child: const Text('Sign out'),
-      );
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          
-          automaticallyImplyLeading: false,
-          iconTheme: const IconThemeData(color: Colors.white),
-          backgroundColor: primaryColor,
-          title: const Text(
-            "My Profile",
-            style: TextStyle(color: Colors.white),
-          ),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const Nav();
-              }));
-            },
-          ),
-        ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: primaryColor,
-        body: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(
-                height: 16,
+        title: const Text(
+          "My Profile",
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return const Nav();
+            }));
+          },
+        ),
+      ),
+      backgroundColor: primaryColor,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 120,
+              width: 120,
+              decoration:  BoxDecoration(
+                shape: BoxShape.circle,
+                image: const DecorationImage(image: AssetImage("assets/luffy.png")),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 3,
+                ),
               ),
-
-              Container(
-                height: 120,
-                width: 120,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image:
-                        DecorationImage(image: AssetImage("assets/luffy.png"))),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-
-              //Name
-              Row(mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(width: 50,),
-                  const Text(
-                    'Monkey D. Luffy',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 24,
-                        color: Colors.white),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // Name
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(width: 50),
+                Text(
+                  user?.displayName ?? 'User name',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24,
+                    color: Colors.white,
                   ),
-                  const SizedBox(width: 10,),
-                   IconButton(color: Colors.white,
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(
+                ),
+                const SizedBox(width: 10),
+                IconButton(
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
                         builder: (context) => const EditProfile(),
                         settings: const RouteSettings(name: 'Edit Profile'),
-                      ));
-                    }, 
-                    icon: const Icon(Icons.mode_edit_outline_outlined))
-                ],
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.mode_edit_outline_outlined),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            // ID TAG
+            const Text(
+              '@nika',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            // Bio
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2,
+                ),
               ),
-              const SizedBox(
-                height: 4,
-              ),
-
-              //ID TAG
-              const Text(
-                '@nika',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-
-              //Social ID Links
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {
-                      
-                    },
-                    child: Container(
-                      height: 24,
-                      width: 24,
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("assets/linky.png"))),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      
-                    },
-                    child: Container(
-                      height: 24,
-                      width: 24,
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("assets/gitboi.png"))),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      
-                    },
-                    child: Container(
-                      height: 24,
-                      width: 24,
-                      decoration: const BoxDecoration(
-                          image:
-                              DecorationImage(image: AssetImage("assets/x.png"))),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-
-              //Bio shi
-              const Text(
-                'I am Going to be the King of the Pirates.\nI am not a hero. I will not share my food.\nBring me MEAT.',
+              child: const Text(
+                'BIO HERE',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 16,
                   color: Colors.white,
                 ),
               ),
-
-              _userUID(),
-              const SizedBox(height: 20,),
-              _signoutButt(),
-              //Portfolio ig
-            ],
-          ),
-        ));
+            ),
+            const SizedBox(height: 16),
+            _userUID(),
+            const SizedBox(height: 20),
+            _signoutButt(),
+          ],
+        ),
+      ),
+    );
   }
 }
